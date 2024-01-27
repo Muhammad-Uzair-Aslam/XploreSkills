95392629f68bf1594058abf27413243a3f6acb71
Uzair Aslam <uzairaslam990@gmail.com>
2024-01-27 00:28:08 -0800

    About page added

---
+++ app/About/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import HeaderImage from '../(components)/headerImage/headerImage'
+
+export default function About() {
+  return (
+    <div>
+      <HeaderImage names="About Us"/>
+    </div>
+  )
+}
