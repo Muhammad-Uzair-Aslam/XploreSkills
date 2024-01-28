93a638fb5a521ff8c7246a77c77a85f6e5c7365a
Uzair Aslam <uzairaslam990@gmail.com>
2024-01-28 00:33:34 -0800

    Blog page added

---
+++ app/Blog/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import HeaderImage from '../(components)/headerImage/headerImage'
+
+export default function Blog() {
+  return (
+    <div>
+      <HeaderImage names="Our Posts"/>
+    </div>
+  )
+}
