ad1d9e0ace7a873bc0d6f9829a8345510379b124
Uzair Aslam <uzairaslam990@gmail.com>
2024-01-29 00:36:07 -0800

    contact page added

---
+++ app/Contact/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import HeaderImage from '../(components)/headerImage/headerImage'
+
+export default function Contact() {
+  return (
+    <div>
+      <HeaderImage names="Contact Us"/>
+    </div>
+  )
+}
