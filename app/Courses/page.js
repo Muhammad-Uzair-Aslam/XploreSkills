0573ce426c0a98397369e898a34c2b9c2fe527c8
Uzair Aslam <uzairaslam990@gmail.com>
2024-01-30 00:38:01 -0800

    courses page added

---
+++ app/Courses/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import HeaderImage from '../(components)/headerImage/headerImage'
+
+export default function Courses() {
+  return (
+    <div>
+      <HeaderImage names="Our Courses"/>
+    </div>
+  )
+}
